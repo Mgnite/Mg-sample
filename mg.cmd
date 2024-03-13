1356de37ab65711b6b4b50c4b332f3d8ab8b05d1
Hisashi Miyashita <himi@mgnite.com>
2024-03-13 16:57:35 -0400

    Added

---
+++ mg.cmd
@@ -0,0 +1,6 @@
+rem Specify the path of your MATLAB.exe
+set MG_MATLAB_EXE_PATH=c:/Program Files/MATLAB/R2021a/bin/matlab.exe
+cd %~dp0
+rem Specify the location of launch.cmd in your Mg installation
+call c:\users\himi\Mg\launch.cmd
+
